fbf6197b80e586daa38909f40277a19b914a5cb9
Aaron Deane <aaronmdeane@gmail.com>
2018-12-11 15:46:08 -0800

    appropriate server response and form input caputured

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 
-// app.static(__dirname)
-app.use(express.static('client'))
+const salesData = [];
+
+const jsonConverter = (object) => {
+    
+}
+
+app.use(express.static('client'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/sales', (req, res, next) => {
-    res.send('Here is the sales report!');
+    res.send(JSON.stringify(req.body));
 });
 
 app.post('/sales', (req, res, next) => {
-    res.statusCode(201).send();
+    res.send(req.body);
 });
 
 app.listen(PORT, ()=>
