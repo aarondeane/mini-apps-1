604162b5dbe2d4184ef2cf054f8275a2decd1d5e
Aaron Deane <aaronmdeane@gmail.com>
2018-12-12 14:17:51 -0800

    Completed server and environment setup, react element rendering

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        }
+    };
+
+    render() {
+        return (
+            <div className="main">
+                <div className="header">
+                    <h1>Welcome to the Checkout</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+// Install express and configure
+const express = require('express');
+const app = express();
+const PORT = 3001;
+const bodyParser = require('body-parser');
+// Setup middlewar
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+// Serve up static files
+app.use(express.static('public'))
 
 
+
+//Set listener
+app.listen(PORT, () => console.log(`Express is now listening on port:${PORT}`));
