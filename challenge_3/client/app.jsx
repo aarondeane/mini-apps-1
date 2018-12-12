fdfd96c0e18cfd80b8667b1b648a8be81d875b52
Aaron Deane <aaronmdeane@gmail.com>
2018-12-12 14:50:24 -0800

    forms created and state on handleChange

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,13 +1,116 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+const F1 = (props) => (
+    <form onSubmit={props.handleSubmit}>
+        <label>
+            First Name:
+            <input type="text" name="firstName" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Last Name:
+            <input type="text" name="lastName" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Email:
+            <input type="text" name="email" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Password:
+            <input type="text" name="pw" onChange={props.handleChange} />
+        </label><br/>
+        <input type="submit" value="Next" />
+    </form>
+)
+
+const F2 = (props) => (
+    <form onSubmit={props.handleSubmit}>
+        <label>
+            Address:
+            <input type="text" name="address1" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Address cont:
+            <input type="text" name="address2" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            City:
+            <input type="text" name="city" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            State:
+            <input type="text" name="state" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Zip Code:
+            <input type="text" name="zip" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Phone Number:
+            <input type="text" name="phone" onChange={props.handleChange} />
+        </label><br/>
+        <input type="submit" value="Next" />
+    </form>
+)
+
+const F3 = (props) => (
+    <form onSubmit={props.handleSubmit}>
+        <label>
+            Credit Card Number:
+            <input type="text" name="cardnum" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Expiration:
+            <input type="text" name="expiration" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            CVV:
+            <input type="text" name="cvv" onChange={props.handleChange} />
+        </label><br/>
+        <label>
+            Billing Zip:
+            <input type="text" name="billzip" onChange={props.handleChange} />
+        </label><br/>
+        <input type="submit" value="Next" />
+    </form>
+)
+
+const Confirm = (props) => {
+    
+}
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
-        }
+            firstName: '',
+            lastName:'',
+            email:'',
+            password:'',
+            address:'',
+            city:'',
+            state:'',
+            zip:'',
+            phone:'',
+            cardnum:'',
+            expire:'',
+            cvv:'',
+            billZip:'',
+        };
+        this.handleChange =  this.handleChange.bind(this);
     };
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+        
+    }
+
+    handleSubmit(event) {
+
+    }
 
     render() {
         return (
@@ -15,6 +118,18 @@ class App extends React.Component {
                 <div className="header">
                     <h1>Welcome to the Checkout</h1>
                 </div>
+                <div className="pagecontent">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="submit" value="Checkout" />
+                    </form>
+                </div>
+                <div>
+                    <F1 handleChange={this.handleChange} handleSubmit={this.handleSubmit} />                
+                    <F2 handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+                    <F3 handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+                </div>
+                
+                
             </div>
         );
     }
