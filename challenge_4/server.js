2892c4a5259a96b2ba061f53e9f33163790f9d43
Aaron Deane <aaronmdeane@gmail.com>
2018-12-13 11:41:09 -0800

    Express server setup

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,17 @@
+// Require Express and create server
+const express = require('express');
+const app = express();
+const PORT = 4000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+app.use(express.static('./client/dist'));
+
+//Define and use middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
 
