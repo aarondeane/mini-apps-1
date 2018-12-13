b43df166e64e3402f8c4680bb184fa57c5bf4d70
Aaron Deane <aaronmdeane@gmail.com>
2018-12-13 11:53:22 -0800

    React components rendering to dom

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -5,12 +5,22 @@ const PORT = 4000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+// Serve up dist files
 app.use(express.static('./client/dist'));
 
-//Define and use middleware
+// Define and use middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
 
+// Server routing
+app.get('/', (req, res, send) => {
+
+});
+
+app.post('/', (req, res, send) => {
+
+});
 
 
 app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
