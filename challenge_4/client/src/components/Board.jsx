840722783a4d371dc45fcb4e24305454dcbd05ff
Aaron Deane <aaronmdeane@gmail.com>
2018-12-13 20:40:12 -0800

    Updated board size and initial piece placement

--- challenge_4/client/src/components/Board.jsx
+++ challenge_4/client/src/components/Board.jsx
@@ -12,22 +12,41 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(56).fill(null),
+            squares: [
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null],
+                [null,null,null,null,null,null]
+                ],
+            // squares: Array(56).fill(null),
             player1Next: true,
         };
         this.handleClick = this.handleClick.bind(this);
     }
-    renderSquare(i) {
-        return (<Square value={this.state.squares[i]}
+    renderSquare(i,j) {
+        return (<Square value={this.state.squares[i][j]}
             // onClick={() => this.handleClick(i)}
             />
         );
     }
     handleClick(e) {
         const squares = this.state.squares.slice();
-        const colId = e.target.parentNode.id;
+        const colId = e.target.parentNode.id.slice(-1);
         const colSquares = e.target.parentNode.children;
-        console.log(colSquares);
+        let squareVals = [];
+
+        for(var i = 0; i < colSquares.length; i++) {
+            if(colSquares[i].outerHTML !== "<br>") {
+              squareVals.push(colSquares[i].value);
+            }
+        }
+
+        if(!squareVals.includes('1') && !squareVals.includes('2')) {
+            squares[colId][5] = this.state.player1Next ? '1' : '2';
+        }
         
         // squares[i] = this.state.player1Next ? '1' : '2';
         this.setState({
@@ -49,77 +68,61 @@ class Board extends React.Component {
         <div>    
             <div className="status">{status}</div><br />
             <div className="container">
+                <div id="col0" className="board-column" onClick={(e) => this.handleClick(e)}>
+                    {this.renderSquare(0,0)}<br />
+                    {this.renderSquare(0,1)}<br />
+                    {this.renderSquare(0,2)}<br />
+                    {this.renderSquare(0,3)}<br />
+                    {this.renderSquare(0,4)}<br />
+                    {this.renderSquare(0,5)}<br />
+                </div>
                 <div id="col1" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(0)}<br />
-                    {this.renderSquare(1)}<br />
-                    {this.renderSquare(2)}<br />
-                    {this.renderSquare(3)}<br />
-                    {this.renderSquare(4)}<br />
-                    {this.renderSquare(5)}<br />
-                    {this.renderSquare(6)}<br />
+                    {this.renderSquare(1,0)}<br />
+                    {this.renderSquare(1,1)}<br />
+                    {this.renderSquare(1,2)}<br />
+                    {this.renderSquare(1,3)}<br />
+                    {this.renderSquare(1,4)}<br />
+                    {this.renderSquare(1,5)}<br />
                 </div>
                 <div id="col2" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(7)}<br />
-                    {this.renderSquare(8)}<br />
-                    {this.renderSquare(9)}<br />
-                    {this.renderSquare(10)}<br />
-                    {this.renderSquare(11)}<br />
-                    {this.renderSquare(12)}<br />
-                    {this.renderSquare(13)}<br />
+                    {this.renderSquare(2,0)}<br />
+                    {this.renderSquare(2,1)}<br />
+                    {this.renderSquare(2,2)}<br />
+                    {this.renderSquare(2,3)}<br />
+                    {this.renderSquare(2,4)}<br />
+                    {this.renderSquare(2,5)}<br />
                 </div>
                 <div id="col3" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(14)}<br />
-                    {this.renderSquare(15)}<br />
-                    {this.renderSquare(16)}<br />
-                    {this.renderSquare(17)}<br />
-                    {this.renderSquare(18)}<br />
-                    {this.renderSquare(19)}<br />
-                    {this.renderSquare(20)}<br />
+                    {this.renderSquare(3,0)}<br />
+                    {this.renderSquare(3,1)}<br />
+                    {this.renderSquare(3,2)}<br />
+                    {this.renderSquare(3,3)}<br />
+                    {this.renderSquare(3,4)}<br />
+                    {this.renderSquare(3,5)}<br />
                 </div>
                 <div id="col4" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(21)}<br />
-                    {this.renderSquare(22)}<br />
-                    {this.renderSquare(23)}<br />
-                    {this.renderSquare(24)}<br />
-                    {this.renderSquare(25)}<br />
-                    {this.renderSquare(26)}<br />
-                    {this.renderSquare(27)}<br />
+                    {this.renderSquare(4,0)}<br />
+                    {this.renderSquare(4,1)}<br />
+                    {this.renderSquare(4,2)}<br />
+                    {this.renderSquare(4,3)}<br />
+                    {this.renderSquare(4,4)}<br />
+                    {this.renderSquare(4,5)}<br />
                 </div>
                 <div id="col5" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(28)}<br />
-                    {this.renderSquare(29)}<br />
-                    {this.renderSquare(30)}<br />
-                    {this.renderSquare(31)}<br />
-                    {this.renderSquare(32)}<br />
-                    {this.renderSquare(33)}<br />
-                    {this.renderSquare(34)}<br />
+                    {this.renderSquare(5,0)}<br />
+                    {this.renderSquare(5,1)}<br />
+                    {this.renderSquare(5,2)}<br />
+                    {this.renderSquare(5,3)}<br />
+                    {this.renderSquare(5,4)}<br />
+                    {this.renderSquare(5,5)}<br />
                 </div>
                 <div id="col6" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(35)}<br />
-                    {this.renderSquare(36)}<br />
-                    {this.renderSquare(37)}<br />
-                    {this.renderSquare(38)}<br />
-                    {this.renderSquare(39)}<br />
-                    {this.renderSquare(40)}<br />
-                    {this.renderSquare(41)}<br />
-                </div>
-                <div id="col7" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(42)}<br />
-                    {this.renderSquare(43)}<br />
-                    {this.renderSquare(44)}<br />
-                    {this.renderSquare(45)}<br />
-                    {this.renderSquare(46)}<br />
-                    {this.renderSquare(47)}<br />
-                    {this.renderSquare(48)}<br />
-                </div>
-                <div id="col8" className="board-column" onClick={(e) => this.handleClick(e)}>
-                    {this.renderSquare(49)}<br />
-                    {this.renderSquare(50)}<br />
-                    {this.renderSquare(51)}<br />
-                    {this.renderSquare(52)}<br />
-                    {this.renderSquare(53)}<br />
-                    {this.renderSquare(54)}<br />
-                    {this.renderSquare(55)}<br />
+                    {this.renderSquare(6,0)}<br />
+                    {this.renderSquare(6,1)}<br />
+                    {this.renderSquare(6,2)}<br />
+                    {this.renderSquare(6,3)}<br />
+                    {this.renderSquare(6,4)}<br />
+                    {this.renderSquare(6,5)}<br />
                 </div>
             </div>
         </div>
@@ -128,12 +131,7 @@ class Board extends React.Component {
 }
 
 function gravity(column) {
-    let values=[];
-    for(var i = 0; i < colSquares.length; i++) {
-        if(colSquares[i].outerHTML !== "<br>") {
-          values.push(colSquares[i].value);
-        }
-    }
+    
 }
 
 function calculateWinner(squares) {
